30e95d463197c655e9566244df256b9f436df2e0
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-12-14 10:10:55 +0100

    Add calculate function to home component Initialize user when opening app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,7 +9,10 @@ export class HomePage {
   user: any = {};
 
   constructor(public navCtrl: NavController) {
-
+    this.user = { distance: 1000, age: 20 };
   }
 
+  calculate() {
+    console.log(this.user);
+  }
 }
