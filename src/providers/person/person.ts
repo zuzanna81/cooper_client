19479f6f9017b6ca830424cd8d3d6117b91e845f
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-12-14 10:29:33 +0100

    Generate provider Person Add method for PersonProvider in person.ts

---
+++ src/providers/person/person.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+import { CooperProvider } from '../cooper/cooper';
+
+@Injectable()
+export class PersonProvider {
+  public gender: string;
+  public age: number;
+  public assessmentMessage: string;
+
+  constructor(private cooper: CooperProvider) {}
+
+  doAssessment(distance: number): void {
+    this.assessmentMessage = this.cooper.assess(this, distance);
+  }
+}
