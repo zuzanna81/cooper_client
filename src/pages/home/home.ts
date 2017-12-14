89b6d2be7d626f91d2c621c4a1352b78846188db
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-12-14 19:46:05 +0100

    Add component for perfomance data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { PersonProvider } from '../../providers/person/person';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PerfomanceDataProvider } from '../../providers/perfomance-data/perfomance-data';
 
 @Component({
   selector: 'page-home',
@@ -11,16 +12,16 @@ export class HomePage {
 
   constructor(
     public navCtrl: NavController,
-    public person: PersonProvider
+    public person: PersonProvider,
+    public perfomanceData: PerfomanceDataProvider
   ) {
     this.user = { distance: 1000, age: 20, gender: 'female' };
   }
 
-  calculate() {
-    this.person.age = this.user.age;
-    this.person.gender = this.user.gender;
-
-    this.person.doAssessment(this.user.distance);
-    console.log(this.person.assessmentMessage);
+  calculate(user) {
+    this.person.doAssessment(user.distance);
+    this.perfomanceData
+      .saveData({ perfomance_data: { data: { message: this.person.assessmentMessage}}})
+      .subscribe(data => console.log(data));
   }
 }
